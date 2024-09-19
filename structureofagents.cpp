8b1d50ca89975a819c93bf23cf83f0c85e69ec76
anyakara <66985689+anyakara@users.noreply.github.com>
2024-09-18 18:23:45 -0700

    Create structureofagents.cpp

---
+++ structureofagents.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+
+class Percept() {
+  
+}
+
+class Action() {
+  private:
+    std::string actionName;
+    std::vector<Percept> percepts; // chain of percepts that are formed while agent is in progress of completing a task
+  // public: attributes to be implemented
+}
+
+// TODO: Implement hash table that contains all the actions with percepts being vectors
+
+// pre-defined sequence of percepts
+Action tableDrivenAgent(Percept percept) {
+  Action flag;
+  
+  std::vector<Percepts> percepts;
+  std::vector<Action> actions; // should rather be a hash table, right now just implementing something
+
+  // append percept to the end of percepts;
+  // action = Lookup(percepts, table);
+  return action;
+}
